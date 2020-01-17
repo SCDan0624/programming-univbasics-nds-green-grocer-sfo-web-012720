2927d34a818978d7c0a74307d324094c65b430b5
Daniel Mayle <dmayle012@gmail.com>
2020-01-17 17:38:03 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -84,6 +84,6 @@ def checkout(cart, coupons)
   counter = 0 
   
   while counter < final_cart.length
-    total += final_cart[counter][:price] * 
+    total += final_cart[counter][:price] * final_cart[counter][:count]
   end
 end
