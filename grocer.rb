81c50545af282ebac2372443e3d248cd7487f51d
Daniel Mayle <dmayle012@gmail.com>
2020-01-17 17:09:51 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -49,7 +49,8 @@ def apply_coupons(cart, coupons)
           cart_item[:count] -= coupons[:counter][:num]
         else
           cart_item_with_coupon = {
-            :item
+            :item = >couponed_item_name,
+            
           }
         end
       end
